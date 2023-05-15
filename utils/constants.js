73c1a1803f7110ff65241e54ff90765e2c759884
Nitsa Saban <nitsnats@hotmail.com>
2023-05-15 03:52:51 +0300

    Change mongoose definition

--- utils/constants.js
+++ utils/constants.js
@@ -5,9 +5,9 @@ const PASS_REGEXP = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
 
 // CONFIG
 const { JWT_SECRET = 'JWT_SECRET' } = process.env;
-const { DB_ADDRESS = 'mongodb://127.0.0.1:27017/newsdb' } = process.env;
+// const { DB_ADDRESS = 'mongodb://127.0.0.1:27017/newsdb' } = process.env;
 // const { DB_ADDRESS = 'mongodb://localhost:27017/newsdb' } = process.env;
-//const DB_ADDRESS = 'mongodb://127.0.0.1:27017/newsdb';
+const DB_ADDRESS = 'mongodb://127.0.0.1:27017/newsdb';
 
 // STATUS CODE
 const ER_MES_OK = 200;
